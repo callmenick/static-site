17489a3aac4850c3b6904983ad45ab16f52d159b
Nick Salloum <nick.salloum@axiomzen.co>
2016-10-10 19:15:47 -0400

    adds site titles and descriptions, and makes ready for variable blocks in views

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   SITE_TITLE: 'Static Site',
-  SITE_DESCRIPTION: 'A static site.',
+  SITE_BLURB: 'A static site',
+  SITE_DESCRIPTION: 'This is a static site, generated with node, gulp, and other shiny things.'
 };
